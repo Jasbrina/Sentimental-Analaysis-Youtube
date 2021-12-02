a6492100bff5c9c6cd18a6fcb4a813397ef202f9
h5q1b <djasbrina@gmail.com>
2021-12-01 17:01:19 -0800

    fifth commit

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,49 +1,71 @@
-import pandas as pd
-import numpy as np 
-import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-import matplotlib.pyplot as plt
 
 
-# removing stopwords
-def remove_stopwords(sentence):
-    stp = set(stopwords.words('english'))
-    word_tokens = word_tokenize(sentence)
+class preprocessing():
+    def __init__(self, sentence):
+        self.sentence = sentence
 
-    filtered_sentence = [w for w in word_tokens if not w.lower() in stp]
-    filtered_sentence = []
-    
-    for w in word_tokens:
-        if w not in stp:
-            filtered_sentence.append(w)
+    # removing stopwords
+    def remove_stopwords(self):
+        stp = set(stopwords.words('english'))
+        word_tokens = word_tokenize(self.sentence)
 
-    s = " ".join(filtered_sentence)
+        filtered_sentence = [w for w in word_tokens if not w.lower() in stp]
+        filtered_sentence = []
+        
+        for w in word_tokens:
+            if w not in stp:
+                filtered_sentence.append(w)
 
-    return s
+        s = " ".join(filtered_sentence)
 
-# converts all words in sentence to lowercase
-def lower_case(sentence):
-    return sentence.lower()
+        self.sentence = s
 
-# 
-def apostrophe(sentence):
-    appos = {
-        "aren't": "are not",
-        "can't": "cannot",
-        "didn't": "did not",
-        "doesn't": "does not",
-        "don't": "do not",
-        "i'll": "I will",
-        "i've": "I have",
-        "couldn't": "could not"
-    }
+    # converts all words in sentence to lowercase
+    def lower_case(self):
+        self.sentence = self.sentence.lower()
 
-    words = sentence.split()
-    reformed = [appos[word] if word in appos else word for word in words]
-    retval = " ".join(reformed)
-    return retval
+    # converts contractions to their full form in a given sentence
+    def remove_contractions(self):
+        appos = {
+            "aren't": "are not",
+            "can't": "cannot",
+            "didn't": "did not",
+            "doesn't": "does not",
+            "don't": "do not",
+            "i'll": "I will",
+            "i've": "I have",
+            "couldn't": "could not"
+        }
+
+        words = self.sentence.split()
+        reformed = [appos[word] if word in appos else word for word in words]
+        retval = " ".join(reformed)
+        self.sentence = retval
+
+    # removes punctuation from a given sentence
+    def remove_punctuation(self):
+        word_tokens = word_tokenize(self.sentence)
+        words = []
+        for word in word_tokens:
+            if word.isalpha():
+                words.append(word)
+        print(words)
+        self.sentence = " ".join(words)
+
+    def preprocess(self):
+        self.lower_case()
+        self.remove_stopwords()
+        self.remove_contractions()
+        self.remove_punctuation()
+
+    def get_sent(self):
+        return self.sentence
 
 
-print(apostrophe(lower_case("Dam this is the cutest and saddest movie I've ever seen I played with my dog for an hour after watching this, couldn't cry")))
+
+# n = preprocessing("Dam this is the cutest and saddest movie I've ever seen I played with my dog for an hour after watching this, couldn't cry")
+# n.preprocess()
+# print(n.get_sent())
